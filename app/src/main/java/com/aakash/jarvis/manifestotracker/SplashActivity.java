3d4ee0a3f0ed8a94478160c8c5d121d5988c13ee
Chirag Aggarwal <chi6rag@gmail.com>
2015-08-30 00:27:51 +0530

    Add Thread based SplashActivity Code

--- app/src/main/java/com/aakash/jarvis/manifestotracker/SplashActivity.java
+++ app/src/main/java/com/aakash/jarvis/manifestotracker/SplashActivity.java
@@ -3,7 +3,6 @@ package com.aakash.jarvis.manifestotracker;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 /**
  * Created by chi6rag on 8/29/15.
@@ -16,37 +15,21 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.splash);
 
         // splash screen code
-        new Handler().postDelayed(new Runnable() {
+        Thread splashScreenThread = new Thread() {
+            public void run(){
+                try {
+                    sleep(3*1000);
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch(Exception e){
 
-            // Using handler with postDelayed called runnable run method
-
-            @Override
-            public void run() {
-                Intent i = new Intent(getBaseContext(), MainActivity.class);
-                SplashActivity.this.startActivity(i);
-
-                // close this activity
-                SplashActivity.this.finish();
+                }
             }
-        }, 5*1000); // wait for 5 seconds
+        };
 
-
-
-//        Thread splashScreenThread = new Thread() {
-//            public void run(){
-//                try {
-//                    sleep(3*1000);
-//                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
-//                    startActivity(intent);
-//                    finish();
-//                }
-//                catch(Exception e){
-//
-//                }
-//            }
-//        };
-//
-//        splashScreenThread.start();
+        splashScreenThread.start();
 
     }
 
